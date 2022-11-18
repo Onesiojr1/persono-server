cb2553d845ae33eeb1c49cbc34cad9ecc97a3af9
Onesio <onesiojr1@gmail.com>
2022-11-18 18:56:57 -0300

    refactor: fixing indentation

--- src/server.ts
+++ src/server.ts
@@ -9,30 +9,26 @@ async function bootstrap() {
   fastify.get("/blogPost", async (request, reply) => {
     const blogPost = await prisma.blogPost.findMany();
 
-    return reply.status(200).send(blogPost );
+    return reply.status(200).send(blogPost);
   });
 
   fastify.post("/blogPost", async (request, reply) => {
+    const body = JSON.parse(request.body);
 
-    const body = JSON.parse(request.body) ;
-
-    
     const blogPost = await prisma.blogPost.create({
       data: {
-          title: body.title,
-          name: body.name,
-          body: body.body,
-          category: body.category
-      }
-  })
+        title: body.title,
+        name: body.name,
+        body: body.body,
+        category: body.category,
+      },
+    });
 
+    return reply.status(201).send({ blogPost });
+  });
 
-    return reply.status(201).send({ blogPost})
-  })
-
-  await fastify.listen({ port: 8000,  host: '0.0.0.0' });
-  console.log('API: 0.0.0.0:8000');
-  
+  await fastify.listen({ port: 8000, host: "0.0.0.0" });
+  console.log("API: 0.0.0.0:8000");
 }
 
 bootstrap();
